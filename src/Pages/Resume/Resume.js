b07ad36ed580d1b4005b7c599eb3a5121cfa8523
Jose_contis <48212689+Josecontis@users.noreply.github.com>
2025-10-04 19:51:59 +0000

    Updated resume page and fixed file download

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { useTranslation } from "react-i18next";
@@ -10,11 +10,19 @@ import Resumecontent from "./ResumeContent";
 
 export const Resume = () => {
   const location = useLocation();
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const resumeLinks = {
+    it: "https://drive.google.com/file/d/1Ii2HQtEBchAlnXpgOVG4iiPS4pk_oAcv/view?usp=sharing",
+    en: "https://drive.google.com/file/d/1lqyWowOpRn-TKHkrHK-em-x2MhJRQaHz/view?usp=sharing"
+  };
+
+  const currentLang = i18n.language; 
 
   useEffect(() => {
-    if (location && location.pathname === "/resume")
+    if (location && location.pathname === "/resume") {
       document.title = `${t("topBar.resume")} | Giuseppe Conticchio`;
+    }
   }, [location, t]);
 
   return (
@@ -24,18 +32,29 @@ export const Resume = () => {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={
-              "https://drive.google.com/file/d/1G8Pm46hPBEIWhq2oPSshQxTIS4gdk6ew/view?usp=sharing"
-            }
+            href={resumeLinks[currentLang] || resumeLinks.it}
             target="_blank"
           >
             <AiOutlineDownload />
             &nbsp;{t("resume.download")}
           </Button>
         </Row>
+
+        {/* 🔹 Contenuto */}
         <Row className="resume">
           <Col md={6} className="resume-left">
             <h3 className="resume-title">{t("resume.experience.title")}</h3>
+
+            <Resumecontent
+              title={t("resume.experience.entries.title2")}
+              date={t("resume.experience.entries.date2")}
+              content={[t("resume.experience.entries.content2")]}
+            />
+            <Resumecontent
+              title={t("resume.experience.entries.title1")}
+              date={t("resume.experience.entries.date1")}
+              content={[t("resume.experience.entries.content1")]}
+            />
             <Resumecontent
               title={t("resume.experience.entries.title")}
               date={t("resume.experience.entries.date")}
@@ -46,11 +65,7 @@ export const Resume = () => {
                 t("resume.experience.entries.content.list3"),
               ]}
             />
-            <Resumecontent
-              title={t("resume.experience.entries.title1")}
-              date={t("resume.experience.entries.date1")}
-              content={[t("resume.experience.entries.content1")]}
-            />
+
             <h3 className="resume-title">
               {t("resume.extracurricular.title")}
             </h3>
@@ -58,23 +73,21 @@ export const Resume = () => {
               title={t("resume.extracurricular.entries.title")}
               content={[t("resume.extracurricular.entries.content")]}
             />
-            <Resumecontent
-              title={t("resume.extracurricular.entries.title1")}
-              content={[t("resume.extracurricular.entries.content1")]}
-            />
           </Col>
+
           <Col md={6} className="resume-right">
             <h3 className="resume-title">{t("resume.education.title")}</h3>
-            <Resumecontent
-              title={t("resume.education.entries.title")}
-              date={t("resume.education.entries.date")}
-              content={[t("resume.education.entries.content")]}
-            />
             <Resumecontent
               title={t("resume.education.entries.title1")}
               date={t("resume.education.entries.date1")}
               content={[t("resume.education.entries.content1")]}
             />
+            <Resumecontent
+              title={t("resume.education.entries.title")}
+              date={t("resume.education.entries.date")}
+              content={[t("resume.education.entries.content")]}
+            />
+
             <h3 className="resume-title">{t("resume.achievements.title")}</h3>
             <Resumecontent
               content={[
@@ -86,12 +99,11 @@ export const Resume = () => {
             />
           </Col>
         </Row>
+
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={
-              "https://drive.google.com/file/d/1G8Pm46hPBEIWhq2oPSshQxTIS4gdk6ew/view?usp=sharing"
-            }
+            href={resumeLinks[currentLang] || resumeLinks.it}
             target="_blank"
           >
             <AiOutlineDownload />
